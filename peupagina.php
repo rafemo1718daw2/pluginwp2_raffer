050cfb8933f17f8d4a6bfadea694eae6507225b9
Rafemo <rafemo1718daw2@gmail.com>
2018-04-20 20:37:59 +0200

    Commit 3

--- peupagina.php
+++ peupagina.php
@@ -4,18 +4,23 @@ Plugin Name: Peu de pàgina per defecte per noves entrades
 Plugin URI: Escriu l'adreça URL completa del teu plugin dins de Github
 Description: Crea peus de pàgina per defecte per a noves entrades
 Version: 0.1
-Author: El teu nom i cognom
+Author: Raul Fernández
 Author URI: L'adreça URL del teu compte de Github
 License: GNU GPLv3
 */
-
 function admin_menu_peu_pagina(){
 $titolPagina="Peu de pàgina per defecte";
 $titolEntrada="Peu de pàgina per defecte";
 $role="administrator";
 $id="peu_pagina_per_defecte";
-add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+$nom_funcio="nouPeuPagDef";
+add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
 }
-add_action('admin_menu','admin_menu_peu_pagina');
+
+function nouPeuPagDef(){
+	include_once('formPeuPag.php');
+}
+
+add_action("admin_menu","admin_menu_peu_pagina");
 ?>
 
